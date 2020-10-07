38ce5b04e726797bc7c3d7e7a6bb527633f6234d
yuuki.fukuda <yuuki.fukuda@dena.com>
2020-10-07 16:00:45 +0900

    small fixes

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,6 @@
 FROM node:10-alpine
 WORKDIR /usr/src/app
 COPY . .
-RUN npm ci
 
 ENV HOST 0.0.0.0
 
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  mode: 'universal',
   loading: false,
   modules: [ '@inkline/nuxt' ],
   plugins: [
